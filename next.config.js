ffd24004c8989388b563b8cfd39ae625630e86dd
Alan Shaw <alan.shaw@protocol.ai>
2022-09-20 06:35:00 -0700

    chore: add build and release ci config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true // TODO: use custom image loader like web3.storage
+  },
 }
 
 module.exports = nextConfig
