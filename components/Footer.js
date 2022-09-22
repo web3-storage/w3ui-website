e6d0adac9dfa482842c5ad6555b4159866b4beb5
Adam Munns <adam@whetinteractive.com>
2022-09-22 14:29:02 -0700

    chore: force build

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
                 href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center gap-6"
+                className="flex items-center gap-5"
             >
                 Powered by Web3.Storage
             </a>
