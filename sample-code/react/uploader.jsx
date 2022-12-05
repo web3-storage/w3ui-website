26a5d7ddb5ac39655f371274f7dc095c0f6f737a
Yusef Napora <yusef@napora.org>
2022-12-05 17:00:36 -0500

    chore: update react uploader example

--- sample-code/react/uploader.jsx
+++ sample-code/react/uploader.jsx
@@ -1,30 +1,33 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useUploader } from '@w3ui/react-uploader'
 
 export default function Component () {
   const [, uploader] = useUploader()
   const [file, setFile] = useState(null)
-  const [cid, setCid] = useState('')
+  const [dataCid, setDataCid] = useState('')
 
   const handleUploadSubmit = async e => {
     e.preventDefault()
     const cid = await uploader.uploadFile(file)
-    setCid(cid)
+    setDataCid(cid)
   }
-  
-  if (cid) {
+
+  if (dataCid) {
     return (
       <div>
         <h1>Done!</h1>
-        <p>{cid}</p>
+        <p>{dataCid.toString()}</p>
+        <p><a href={`https://w3s.link/ipfs/${dataCid}`}>View {file.name} on IPFS Gateway.</a></p>
       </div>
     )
   }
 
   return (
     <form onSubmit={handleUploadSubmit}>
-      <label htmlFor='file'>File:</label>
-      <input id='file' type='file' onChange={e => setFile(e.target.files[0])} required />
+      <div>
+        <label htmlFor='file'>File:</label>
+        <input id='file' type='file' onChange={e => setFile(e.target.files[0])} required />
+      </div>
       <button type='submit'>Upload</button>
     </form>
   )
