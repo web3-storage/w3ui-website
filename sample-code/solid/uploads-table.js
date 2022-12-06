14a10ddba16fc112ff90ab07d13fb457d736d3f7
Yusef Napora <yusef@napora.org>
2022-12-06 11:06:39 -0500

    chore: update solid uploads-table example

--- sample-code/solid/uploads-table.js
+++ sample-code/solid/uploads-table.js
@@ -1,42 +1,45 @@
-import { AuthProvider, useAuth } from '@w3ui/solid-keyring'
+import { Switch, Match } from 'solid-js'
+import { useKeyring } from '@w3ui/solid-keyring'
 import { createUploadsListResource } from '@w3ui/solid-uploads-list'
 
 export default function Component () {
-  const [auth] = useAuth()
-  const [data, { refetch }] = createUploadsListResource(() => auth.identity, { initialValue: { results: [] } })
+  const [keyringState, keyringActions] = useKeyring()
+  const [data, { refetch }] = createUploadsListResource(() => ({...keyringState, ...keyringActions}), { initialValue: { results: [] } })
 
   return (
     <div>
       <Switch>
         <Match when={data.state === 'errored'}>
-          <p>⚠️ {err.message}</p>
+          <p>⚠️ {err.message}</p> 
         </Match>
         <Match when={data.state === 'ready'}>
           {data().results.length
             ? (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Data CID</th>
-                    <th>CAR CID</th>
-                    <th>Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data().results.map(({ dataCid, carCids, uploadedAt }) => (
-                    <tr key={dataCid}>
-                      <td>{dataCid}</td>
-                      <td>{carCids[0]}</td>
-                      <td>{uploadedAt.toLocaleString()}</td>
+              <div>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Data CID</th>
+                      <th>CAR CID</th>
+                      <th>Date</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {data().results.map(({ dataCid, carCids, uploadedAt }) => (
+                      <tr key={dataCid}>
+                        <td>{dataCid}</td>
+                        <td>{carCids[0]}</td>
+                        <td>{uploadedAt.toLocaleString()}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
               )
             : <p>No uploads</p>}
         </Match>
       </Switch>
-      <button type='button' onClick={refetch}>🔄 Refresh</button>
+      <button type='button' onClick={refetch}>Refresh</button>
       {data.loading ? <p>Loading...</p> : null}
     </div>
   )
